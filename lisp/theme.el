34155f8919129eb5009bab2fdbe3664b10e295aa
Henry Matheisen <henry.mthsn@gmail.com>
2019-01-29 17:01:23 +0100

    added elpa packages

--- lisp/theme.el
+++ lisp/theme.el
@@ -4,18 +4,19 @@
 ;; (require 'moe-theme)
 
 ;; ;; Show highlighted buffer-id as decoration. (Default: nil)
-;; (setq moe-theme-highlight-buffer-id t)
+;; ;; (setq moe-theme-highlight-buffer-id t)
 
 ;; ;; Resize titles (optional).
 ;; (setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
 ;; (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
 ;; (setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
+
 ;; (powerline-moe-theme)
 
+;; (moe-dark)
+ 
 ;; (setq show-paren-style 'expression)
 
-;; (moe-dark)
+;; (require 'darktooth-theme)
 
-(require 'darktooth-theme)
-
-(darktooth-modeline)
+;; (darktooth-modeline)
