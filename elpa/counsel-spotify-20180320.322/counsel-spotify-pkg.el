1943c5596ba640b0e9bbf811ed60a78e63765340
Henry Matheisen <henry.mthsn@gmail.com>
2019-01-29 17:07:35 +0100

    deleted elpa/

--- elpa/counsel-spotify-20180320.322/counsel-spotify-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel-spotify" "20180320.322" "Control Spotify search and select music with Ivy." '((emacs "25") (ivy "0.9.0")) :commit "9033e207dccdfea7fe590d2e102d50fcd2bd22e3" :authors '(("Lautaro García <https://github.com/Lautaro-Garcia>")) :maintainer '("Lautaro García <https://github.com/Lautaro-Garcia>"))
